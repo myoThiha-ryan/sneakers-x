5771f7a37cb8f910a060337ac39cce3177d2798b
Myo Thiha <mth2682004@gmail.com>
2025-02-13 23:35:01 +0000

    created super quality section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -14,17 +14,19 @@ const Hero = () => {
     >
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
         <p className="text-xl font-montserrat text-coral-red">
-          Our Summer Collection
+          Our Summer collections
         </p>
-        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
-            The New Arrivals
+            The New Arrival
           </span>
           <br />
           <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
         </h1>
         <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
-          Discover the latest arrivals of sneakers from different brands.
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
         </p>
         <Button label="Shop Now" iconURL={arrowRight} />
         <div className="flex justify-start items-start flex-wrap gap-16 w-full mt-20">
