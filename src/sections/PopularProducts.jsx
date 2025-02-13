0955442cd02f85fbcbc7af63bbb9c1216ec3bd7d
Myo Thiha <mth2682004@gmail.com>
2025-02-13 23:08:55 +0000

    created popular sneakers section

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { products } from "../constants/index.js";
+import PopularProductCard from "../components/PopularProductCard.jsx";
 
 const PopularProducts = () => {
-  return <div>PopularProducts</div>;
+  return (
+    <section id="products" className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="text-4xl font-palanquin font-bold">
+          Our <span className="text-coral-red">Trending</span> Sneakers
+        </h2>
+        <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">
+          Get ahead of the trend with exclusive sneakers designed to turn heads
+          and break limits!
+        </p>
+      </div>
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
+        {products.map((product) => (
+          <PopularProductCard key={product.name} {...product} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default PopularProducts;
