28ada7bdf67f41af7ec1186e720f20f1cefa540d
Myo Thiha <mth2682004@gmail.com>
2025-02-13 15:21:34 +0000

    created sections and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-function App() {
+import React from "react";
+
+const App = () => {
   return (
-    <>
-      <h1 className="text-blue-300 font-bold">Hello World</h1>
-    </>
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+      <section className="padding">Popular Products</section>
+      <section className="padding">Super Quality</section>
+      <section className="padding-x py-10">Services</section>
+      <section className="padding">Special Offers</section>
+      <section className="bg-pale-blue padding">Customer Reviews</section>
+      <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+      <section className="bg-black padding-x padding-t pb-8">Footer</section>
+    </main>
   );
-}
+};
 
 export default App;
